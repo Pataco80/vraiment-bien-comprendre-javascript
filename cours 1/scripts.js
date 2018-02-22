39db30e7a02739be64a3371d445998775e3a4876
Pataco80 <pataco80@gmail.com>
2018-02-22 14:41:38 +0100

    oups, j'avais oublié d'enlever le var à la ligne 5

--- cours 1/scripts.js
+++ cours 1/scripts.js
@@ -2,7 +2,7 @@
 var x;
 console.log(x);
 
-var x = 5; // Message undefined. Javascript à pris lors de ça première lecture, la variable x (la déclaration de la variable)mais attention ! Pas ça valeur
+x = 5; // Message undefined. Javascript à pris lors de ça première lecture, la variable x (la déclaration de la variable)mais attention ! Pas ça valeur
 /* Dans ce commit on voit ce qu'a réellement fait javascript. il à déclaré x, 
 il affiche x dans la console, et seulement après il voit la valeur de x. 
 C'est pour cela que le console.log dit undefined*/
