8dbc362450cad5b47bca97ae16e5d7993c32963d
Pataco80 <pataco80@gmail.com>
2018-02-22 14:36:23 +0100

    valeur de x undefined

--- cours 1/scripts.js
+++ cours 1/scripts.js
@@ -1,1 +1,5 @@
 //Hoisting de variables
+
+console.log(x);
+
+var x = 5; // Message undefined. Javascript à pris lors de ça première lecture, la variable x (la déclaration de la variable)mais attention ! Pas ça valeur
