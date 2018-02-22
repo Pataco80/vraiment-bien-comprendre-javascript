ee66fb9c7f3ebdb200327e68e975c29c7be116b9
Pataco80 <pataco80@gmail.com>
2018-02-22 15:51:00 +0100

    vidéo type primitif et objets first commit

--- cours 1/scripts.js
+++
@@ -1,8 +0,0 @@
-//Hoisting de variables
-var x;
-console.log(x);
-
-x = 5; // Message undefined. Javascript à pris lors de ça première lecture, la variable x (la déclaration de la variable)mais attention ! Pas ça valeur
-/* Dans ce commit on voit ce qu'a réellement fait javascript. il à déclaré x, 
-il affiche x dans la console, et seulement après il voit la valeur de x. 
-C'est pour cela que le console.log dit undefined*/
