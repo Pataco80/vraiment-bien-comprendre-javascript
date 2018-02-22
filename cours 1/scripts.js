f25e64a353200ad79c0326213065c4e1074ead03
Pataco80 <pataco80@gmail.com>
2018-02-22 14:32:26 +0100

    hoisting de variables first commit

--- cours 1/scripts.js
+++ cours 1/scripts.js
@@ -1,9 +1,1 @@
-/* function addition(a, b) {
-    console.log(a + b);
-} */
-
-var addition = function(a, b) {
-    console.log(a + b);
-}; // fonction anonyme dans une variable cela ne fonctionne pas.
-
-addition(5, 7); // Dans cet ordre le code fonctionne car javascript hisse les déclarations de fonctions en haut
+//Hoisting de variables
