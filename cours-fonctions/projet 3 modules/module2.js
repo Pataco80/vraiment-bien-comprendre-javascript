12f067ca19f30dba40ed185baaede56b9ca64f38
Pataco80 <pataco80@gmail.com>
2018-02-25 18:34:47 +0100

    commantaires sur le console.log module 3.js

--- cours-fonctions/projet 3 modules/module2.js
+++ cours-fonctions/projet 3 modules/module2.js
@@ -1,1 +1,12 @@
-var myPassword = "00000";
+/*
+(function() {
+    var myPassword = "00000";
+}());
+*/
+
+// Dans un scope de bloc avec une variable const ou let, cela fait la même chose.
+// Attention ! Ne pas utiliser le var car sinon il y a hoisting 
+{
+    let myPassword = "00000";
+
+}
