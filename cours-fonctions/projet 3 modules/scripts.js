3249c51c4da3d4cdc7b965e2716e634e779df0ba
Pataco80 <pataco80@gmail.com>
2018-02-25 18:21:42 +0100

    résultat du premier test de IIFEs

--- cours-fonctions/projet 3 modules/scripts.js
+++ cours-fonctions/projet 3 modules/scripts.js
@@ -2,4 +2,8 @@
 
 (function nomFonction() {
     //code de la fonction
+    var myName = "Ricardo";
 })();
+/* Les variables déclarées dans une fonction de type IIFEs restent à l'intérieur
+du scope de la fonction et ne sont pas connues du reste du code. Elles sont donc privées*/
+console.log(myName); // Résultat: MyName is not defined
