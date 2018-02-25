3a50f5e98cf4b57f32c5ba307a4f1d97ffe1aae5
Pataco80 <pataco80@gmail.com>
2018-02-25 18:48:33 +0100

    privatisation du module1.js en le mettant intégralement dans une IIFEs

--- cours-fonctions/projet 3 modules/module1.js
+++ cours-fonctions/projet 3 modules/module1.js
@@ -1,12 +1,13 @@
-var myPassword = "12345";
+(function() {
+    var myPassword = "12345";
 
-function setPassword(newPassword) {
-    myPassword = newPassword;
-}
+    function setPassword(newPassword) {
+        myPassword = newPassword;
+    }
 
-function getPassword() {
-    return myPassword;
-}
-//
-//
-//
+    function getPassword() {
+        return myPassword;
+    }
+}()); // Dans cet état, tout le module à été rendu privé. 
+
+// Fin du code
